6408d5796e6aa1485b63d51b021cfd99edd3da87
Select Anchors <selectanchors@gmail.com>
2025-10-03 11:39:53 -0600

    Delete components directory

--- components/AuthLayout.js
+++
@@ -1,9 +0,0 @@
-export default function AuthLayout({ children }) {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50 px-4">
-      <div className="w-full max-w-md">
-        {children}
-      </div>
-    </div>
-  );
-}
