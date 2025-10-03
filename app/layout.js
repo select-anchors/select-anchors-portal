0629adcde4cffb459a78c24570ec18d90ab29514
Select Anchors <selectanchors@gmail.com>
2025-10-03 11:47:06 -0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 
 import "./globals.css";
-import Header from "../components/Header";
+import Header from "./components/Header";
 export const metadata = { title: "Select Anchors Portal", description: "Client login & operations" };
 export default function RootLayout({ children }) {
   return (
