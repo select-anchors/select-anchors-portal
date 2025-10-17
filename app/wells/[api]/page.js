d66f1fca5853e30db5b42ab4dc4d3ace1adf73d8
Select Anchors <selectanchors@gmail.com>
2025-10-17 16:42:07 -0600

    Refactor WellDetail component rendering
    Updated the rendering of well details in the component.

--- app/wells/[api]/page.js
+++ app/wells/[api]/page.js
@@ -2,12 +2,11 @@ export default async function WellDetail({ params }) {
   const { api } = params;
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/wells/${api}`);
   const well = await res.json();
-  
+
   return (
     <div>
-      <h1>{well.lease_name}</h1>
-      <p>Company: {well.company}</p>
-      {/* render the rest of the well info */}
+      <h1>Well: {well.lease_name}</h1>
+      {/* Render well details here */}
     </div>
   );
 }
