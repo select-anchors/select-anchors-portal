36557a363ce42e405436dc0f40139b593861e9da
Select Anchors <selectanchors@gmail.com>
2026-02-26 16:27:49 -0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -14,10 +14,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-        {/* <Providers> */}
-  {/* <Header /> */}
-  {children}
-{/* </Providers> */}
+        <Providers>
+          <Header />
+          {children}
+        </Providers>
       </body>
     </html>
   );
