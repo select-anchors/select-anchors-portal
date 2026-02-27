f0ea98d4ccf8715198886930d5ef40780a2c4170
Select Anchors <selectanchors@gmail.com>
2026-02-26 18:32:44 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
+// next.config.js
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  productionBrowserSourceMaps: true,
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname);
+    return config;
+  },
 };
 
 module.exports = nextConfig;
