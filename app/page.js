18e19784a8553c1d2449b4d973e7fcaa4afc425d
Select Anchors <selectanchors@gmail.com>
2026-03-09 15:09:45 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,10 +3,9 @@ export const dynamic = "force-dynamic";
 
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-import { authOptions } from "@/lib/nextauth-options";
+import { authOptions } from "../lib/nextauth-options";
 
 export default async function Home() {
-  // Be defensive: don't crash build if something throws
   const session = await getServerSession(authOptions);
 
   if (!session?.user?.role) {
@@ -24,6 +23,5 @@ export default async function Home() {
       redirect("/login");
   }
 
-  // Should never hit, but keeps the function well-formed
   return null;
 }
